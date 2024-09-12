2d17cacbccde469f50f10f4f4add8fb7e09cab33
besher al addam <beshoux@outlook.fr>
2024-09-12 22:35:31 +0200

    refactor: Update file structure and components for quiz functionality.
    chore: Show options for each question.
    
    Display the first question qfter pressing the start button.

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initalState = {
   questions: [],
   // status: "ready", "loading", "error", "active", "finished"
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -17,6 +19,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Invalid action type");
   }
@@ -24,7 +28,7 @@ function reducer(state, action) {
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initalState);
-  const { questions, status } = state;
+  const { questions, status, index } = state;
 
   const numQuestions = questions.length;
 
@@ -48,7 +52,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
